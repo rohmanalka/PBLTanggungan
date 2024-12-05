2bff778de9eae4b5c416765938d18d1c261a3e7a
IvanRizal <ivanrizal36@gmail.com>
2024-12-05 20:35:51 +0700

    update login page

--- index.php
+++ index.php
@@ -25,39 +25,60 @@ if (isset($_GET['error'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css//index/style.css">
+    <title>Login Page SimasBeTa</title>
 </head>
 
 <body>
-    <div class="container" style="width: 800px; height: 450px; padding: 20px;">
-        <div class="row">
-            <div class="col-md-6 d-flex flex-column" style="border-right: 1px solid black;">
-                <h2 class="title">Sistem Informasi<br>Bebas Tanggungan</h2>
-                <p class="keterangan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin turpis ligula, placerat accumsan mi luctus vitae. Morbi semper id metus et vulputate. Pellentesque molestie vehicula magna, in vehicula felis ultricies at. Nullam est nunc, pharetra nec sollicitudin non, aliquet a velit. Morbi consectetur, ex eget iaculis ultrices, orci felis sagittis magna, nec pretium tortor tortor at odio. Sed ex turpis, imperdiet eget condimentum eget, laoreet ut eros. Sed lacinia tristique ornare. Phasellus efficitur, eros nec consequat pellentesque, est nibh semper.</p>
-            </div>
-            <div class="col-md-6 d-flex flex-column" style="border-left: 1px solid black;">
-                <img src="assets/img/brand.png" alt="SIMASBETA Logo" width="200">
-                <h2>Hello, Welcome Back</h2>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Contact Us!</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                </div>
+                <p>Admin Rohman Al-Kautsar</p>
+
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="controllers/loginController.php" method="post">
+                <img src="assets/img/brand.png">
                 <p>Login to access your account</p>
                 <div class="alert alert-danger" role="alert">
                     Masukkan username dan password<br>
                     (Menggunakan NIM sebagai username)
                 </div>
-                <div class="login-container">
-                    <form action="controllers/loginController.php" method="post">
-                        <div class="mb-3">
-                            <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>About Us</h1>
+                    <p>Sistem Informasi Bebas Tanggungan (SIBT) adalah sistem yang dirancang untuk mengelola dan memverifikasi status tanggungan seseorang, baik dalam konteks administrasi pemerintah maupun keuangan.</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Welcome Back</h1>
+                    <p>Klik Lanjut untuk Mengetahui Selengkapnya...</p>
+                    <button class="hidden" id="register">Lanjut</button>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/css/index/script.js"></script>
 </body>
+
 </html>
